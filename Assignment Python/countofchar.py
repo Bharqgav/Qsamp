11850c1dbbde15f26a3ce943ce36c5174e91792a
Bharqgav <saisatyabhargav.pothula@quantiphi.com>
2017-07-17 03:37:28 +0530

    Assignment

--- Assignment Python/countofchar.py
+++ Assignment Python/countofchar.py
@@ -1,14 +1,14 @@
 import sys
 a = raw_input("Enter a string")
-norepeat = ''
+norepeat = '' #Empty string
 for char in a:
     x = 0
-    b = char.lower()
+    b = char.lower() #This assumes if the string has an upper and lower case of same letter then it is counted as a repeat
    
     for char in a:
         
-        if char.lower() == b.lower():
-            x = x+1
+        if char.lower() == b.lower(): #to find if b repeats
+            x = x+1 #x returns the number of times char apperas
     if not b in norepeat:
         print b, x
-    norepeat = b + norepeat
+    norepeat = b + norepeat #If a character is appearing more than once, then it appears only once
