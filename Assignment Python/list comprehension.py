973cab345ae2e530c4e4d57923555ecbee21751a
Bharqgav <saisatyabhargav.pothula@quantiphi.com>
2017-07-17 03:30:47 +0530

    Upgrade

--- Assignment Python/list comprehension.py
+++ Assignment Python/list comprehension.py
@@ -12,3 +12,17 @@ for x in c:
     if not x==" ": #To create a list without the spaces created to replace the numbers which are to be removed
         l.append(x)
 print l
+
+#Alternate code
+
+import sys
+a = raw_input("Enter a list of numbers")
+c = a.split() #storing the string input as a list 
+l = [] #Creating an empty list
+
+b = [0,1,2,3] #counting the indexes inorder to remove 0,2,4,6th elements
+for i in b:
+         c.remove(c[i])
+         
+    
+print c
