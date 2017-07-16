4ddbf13e7cd6229d9168bea022be13e1f7cbaf6d
Bharqgav <saisatyabhargav.pothula@quantiphi.com>
2017-07-17 03:21:37 +0530

    Assignment

--- Assignment Python/list comprehension.py
+++ Assignment Python/list comprehension.py
@@ -1,9 +1,14 @@
 #list comprehension
 import sys
-a = map (int ,raw_input(("Enter a list of numbers").split())
-c = a[]
-b = [0,2,4,6]
-for b:
-         c = a.remove(a[b])
+a = raw_input("Enter a list of numbers")
+c = a.split() #storing the string input as a list 
+l = [] #Creating an empty list
 
-print c
+b = [0,2,4,6]
+for i in b:
+         c.remove(c[i])
+         c.insert(i," ") #replacing the removed numbers with spaces so that the inex count does not change
+for x in c:
+    if not x==" ": #To create a list without the spaces created to replace the numbers which are to be removed
+        l.append(x)
+print l
